2e5348966402494950879d71f7a3fbeebb0a290f
Matthew Zamudio <143620932+mattzam@users.noreply.github.com>
2023-11-07 22:06:58 -0600

    Add files via upload

---
+++ P2ArrBidi.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p2arrbidi;
+
+/**
+ *
+ * @author expec
+ */
+public class P2ArrBidi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        char[][]letras = new char[3][5];
+        char letra = 'a';
+        for (int i = 0; i < letras.length; i++) {
+            for (int j = 0; j < letras[0].length; j++) {
+                letras[i][j] = letra;
+                letra++;
+            }
+        }
+        for (int i = 0; i < letras.length; i++) {
+            for (int j = 0; j < letras[0].length; j++) {
+                System.out.print(letras[i][j]+" ");
+            }
+            System.out.println("");
+        }*/
+
+    }
+    
+}
